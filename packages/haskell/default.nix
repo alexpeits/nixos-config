a05a4220b03c01569d9b4d553e4e60a971186c11
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2020-08-03 10:45:24 +0100

    update nixpkgs-unstable

--- packages/haskell/default.nix
+++ packages/haskell/default.nix
@@ -13,17 +13,6 @@ let
     version = "8.10.0.15";
     sha256 = "0ll8zcby70zz1x0i7cskfxkl48mfaw303a3rsz7j62i4h3kq7xzj";
   };
-  # not used
-  ghc-lib-parser-8-10 = hsLib.fetchFromHackage {
-    name = "ghc-lib-parser";
-    version = "8.10.1.20200523";
-    sha256 = "1fnhqb9l0cg58lalrrn4ms48rnnzlyb7dqa9h2g21m9287q5y6gs";
-  };
-  ghc-lib-parser-ex = hsLib.fetchFromHackage {
-    name = "ghc-lib-parser-ex";
-    version = "8.8.2";
-    sha256 = "0ff1rb53wmbkbdral725brs0wg2bg4x2bb2klfwa2cqix1qi68lv";
-  };
 
   brittany = pkgs.callPackage ./brittany.nix {};
   fast-tags = pkgs.callPackage ./fast-tags.nix {};
