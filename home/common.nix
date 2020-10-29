bf7033e9a4335fe1500b37217656fd7f933e40f1
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2020-10-29 11:27:25 +0000

    add fenv fish plugin

--- home/common.nix
+++ home/common.nix
@@ -81,6 +81,17 @@ in
     interactiveShellInit = fishrc.shellInit;
     promptInit = fishrc.promptInit;
     shellAliases = profile.aliases;
+    plugins = [
+      {
+        name = "plugin-foreign-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-foreign-env";
+          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+        };
+      }
+    ];
   };
 
   programs.bash = {
