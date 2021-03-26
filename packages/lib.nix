3327427577a242a60ae21916a1177431d7f49636
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2021-03-26 08:47:35 +0000

    use node-packages json description for node2nix

--- packages/lib.nix
+++ packages/lib.nix
@@ -4,29 +4,28 @@ let
 
   runNode2nix = { pkg, version ? null, node-version ? 12 }:
     let
-      package =
-        if isNull version
-        then ''"${pkg}"''
-        else ''{ "${pkg}": "${version}" }'';
-      showVersion = if isNull version then "" else "-${version}";
+      node-pkg = if isNull version then pkg else { "${pkg}" = version; };
+      pkg-ver = if isNull version then pkg else "${pkg}-${version}";
+      node-packages =
+        pkgs.writeText "node-packages-${pkg-ver}" (builtins.toJSON [ node-pkg ]);
     in
     pkgs.stdenv.mkDerivation {
-      name = "node2nix-${pkg}-${version}";
+      name = "node2nix-${pkg-ver}";
       buildInputs = [ pkgs.nodePackages.node2nix ];
       phases = [ "installPhase" ];
       installPhase = ''
         mkdir -p $out
         cd $out
-        node2nix -i <(echo '[${package}]') --nodejs-${toString node-version}
+        node2nix -i ${node-packages} --nodejs-${toString node-version}
       '';
     };
 
   buildNodePackage = { pkg, version ? null, node-version ? 12 }:
     let
       drv = runNode2nix { pkg = pkg; version = version; node-version = node-version; };
-      package = if isNull version then pkg else "${pkg}-${version}";
+      attr = if isNull version then pkg else "${pkg}-${version}";
     in
-    (pkgs.callPackage drv { })."${package}";
+    (pkgs.callPackage drv { })."${attr}";
 
 in
 
